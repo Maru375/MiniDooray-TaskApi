ff1fbac54f9ab340e8c949fdc821604c40ba132b
Maru375 <www980304@gmail.com>
2024-02-22 09:51:39 +0900

    feat: first commit

---
+++ src/main/java/com/nhnacademy/minidooray/taskapi/TaskapiApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidooray.taskapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhnacademy/minidooray/taskapi/TaskapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidooray.taskapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
