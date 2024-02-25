2d5facd78418b6bd05d441096afb47337da1ea69
Maru375 <www980304@gmail.com>
2024-02-25 16:35:11 +0900

    feat: Create task controller

--- src/main/java/com/nhnacademy/minidooray/taskapi/dto/project/ProjectUpdateRequest.java
+++ src/main/java/com/nhnacademy/minidooray/taskapi/dto/project/ProjectUpdateRequest.java
@@ -6,8 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotNull;
-
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
--- src/main/java/com/nhnacademy/minidooray/taskapi/dto/task/TaskUpdateRequest.java
+++ src/main/java/com/nhnacademy/minidooray/taskapi/dto/task/TaskUpdateRequest.java
@@ -7,8 +7,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotNull;
-
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
